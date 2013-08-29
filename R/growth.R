ae7ae90075a192de4534feae407a4d302d0e0b16
Daniel Rodriguez Perez <daniel.rodriguez.perez@gmail.com>
2013-08-29 00:00:00 +0000

    version 1.0.0

---
+++ R/growth.R
@@ -0,0 +1,24 @@
+#' growthmodels: Nonlinear Growth Models
+#'
+#' A compilation of nonlinear growth models used in many areas.
+#' 
+#' \tabular{ll}{
+#'   Package: \tab growthmodels \cr
+#'   Version: \tab 1.0.0 \cr
+#'   License: \tab GPL-3 \cr
+#' }
+#'
+#' @author Daniel Rodriguez Perez \email{daniel.rodriguez.perez@@gmail.com}
+#' 
+#' @references
+#' D. Fekedulegn, M. Mac Siurtain, and J. Colbert, "Parameter estimation of
+#' nonlinear growth models in forestry," Silva Fennica, vol. 33, no. 4, pp.
+#' 327-336, 1999.
+#' 
+#' http://en.wikipedia.org/wiki/Generalised_logistic_function
+#' 
+#' @name growthmodels-package
+#' @aliases growthmodels
+#' @docType package
+#' 
+NULL
